dbb6a30d149c23b76fe80eff1f6a1190ad32a3d9
pthanhle <22110228@student.hcmute.edu.vn>
2025-04-13 16:41:21 +0700

    load category

--- src/main/java/com/example/demo/config/DatabaseConfig.java
+++ src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -2,23 +2,21 @@ package com.example.demo.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
-
-
 @Configuration
 public class DatabaseConfig {
-	
+
+	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/courseonline?useSSL=false&serverTimezone=UTC");
 		dataSource.setUsername("root");
-		dataSource.setPassword("123456");
-		
-		return dataSource();
-	}
+		dataSource.setPassword("");
 
+		return dataSource;
+	}
 }
